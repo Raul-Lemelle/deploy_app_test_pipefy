2e353cfd00ea102e99d642fe120ab184f0454064
${GITHUB_USER} <${GITHUB_MAIL}>
2024-12-01 20:20:30 -0300

    Primeiro commit?

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/deploy_app_test_pipefy/",
   build: {
-    outDir: "build"
+    outDir: "dist",
   },
   server: {
     host: "0.0.0.0",
     port: 3000,
     strictPort: true,
+    https: {
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem'),
+    },
     hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
-  }
+      clientPort: 443, // Run the websocket server on the SSL port
+    },
+  },
 });
